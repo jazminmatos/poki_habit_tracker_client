65b602ebd71b42d0fa80539341e7de92e016e603
jazminmatos <jmatos1993@gmail.com>
2021-09-23 19:19:24 -0400

    successfully mounted NavBar onto Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom'
 
+import NavBar from './NavBar';
 import Home from './Home';
 import About from './About';
 import Profile from './Profile';
@@ -8,12 +9,15 @@ import Store from './Store';
 
 const Router = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/profile' component={Profile} />
-            <Route exact path='/store' component={Store} />
-        </Switch>
+        <>
+            <NavBar />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/profile' component={Profile} />
+                <Route exact path='/store' component={Store} />
+            </Switch>
+        </>
     );
 }
 
