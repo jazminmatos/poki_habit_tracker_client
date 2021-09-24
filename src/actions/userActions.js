fbb1d05ca90dbed01e3bc763a8eee8c343eb4f45
jazminmatos <jmatos1993@gmail.com>
2021-09-23 22:47:05 -0400

    findOrCreateUser successfully persisted data

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,11 +1,11 @@
 export const findOrCreateUser = user => {
     return dispatch => {
-        fetch('http://127.0.1:3000/users', {
+        fetch('http://localhost:3000/users', {
             method: 'POST',
             body: JSON.stringify(user),
             headers: { 'Content-Type': 'application/json' }
         })
         .then(resp => resp.json())
-        .then(user => dispatchEvent({ type: 'FIND_OR_CREATE_USER', payload: user}))
+        .then(user => dispatch({ type: 'FIND_OR_CREATE_USER', payload: user}))
     }
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,7 +2,7 @@ export const usersReducer = (state = [], action) => {
     switch(action.type) {
         case 'FETCH_USER':
             return action.payload
-        case 'CREATE_USER':
+        case 'FIND_OR_CREATE_USER':
             return console.log(action.payload)
         default: 
             return state
