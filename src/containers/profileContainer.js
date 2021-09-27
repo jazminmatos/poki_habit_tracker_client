c7f2236046a55a8b1628d847e2ce408a0687f1bb
jazminmatos <jmatos1993@gmail.com>
2021-09-27 03:44:25 -0400

    next steps for profile container

--- src/containers/profileContainer.js
+++ src/containers/profileContainer.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Pokemon from '../components/Pokemon';
 import HealthXpContainer from './HealthXpContainer';
 
-const ProfileContainer = () => {
+const ProfileContainer = (props) => {
+    console.log("inside ProfileContainer:", props.users)
+    // ADD LOGIC THAT SAYS THAT USER IS THE ONE WHOSE isLoggedIn ATTRIBUTE IS TRUE
+    // const user = props.users.filter((u) => u.isLoggedIn)
+
+    // Once isLoggedIn attribute is updated, add the following below:
+    // Hello {user.name}
     return (
         <div>
-            Hello from Profile Container 
+            Hello Profile Container 
             <Pokemon />
             <HealthXpContainer />
         </div>
     );
 }
 
-export default ProfileContainer;
+const mapStateToProps = state => {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps)(ProfileContainer);
+
+// Things I need to pass down 
+// Pokemon => pokemon picture, pokemon name
+// HealthXpContainer => health amount, xp amount, coin amount
+
+// How do I get access to the above?
+// Redux via store via connect via mapStateToProps & mapDispatchToProps 
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -3,7 +3,7 @@ export const usersReducer = (state = [], action) => {
     switch(action.type) {
         case 'FIND_OR_CREATE_USER':
             console.log("inside FIND_OR_CREATE_USER reducer", action.payload)
-            return [...state, action.payload]
+            return [...state, action.payload] // need to check whether state exists, if it does, don't add it in
         default: 
             return state
     }
